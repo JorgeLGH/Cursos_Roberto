6cd7ded26ce04bf0b8c8640aa6ff943b8b44a9fd
user <joorgeluisgomezgmail.com>
2022-01-09 19:53:25 -0600

    some things i did the same day

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cursos_Roberto
 Breves cursos de lo que Roberto enseña a alumnos que queremos trabajar con él.
+Esto es una modificación del README a través de la copia local de mi script en mi computadora
+
---
+++ operaciones.R
@@ -0,0 +1,34 @@
+general<- function(a,b,c){
+  if((2*a)>0){
+   pos<- (-b+(sqrt(b^2*-4*a*c)))/2*a
+   neg<- (-b-(sqrt(b^2*-4*a*c)))/2*a
+   print(paste("tus valores son", pos, "y", neg))
+  }
+}
+
+general(3,2,3)
+
+sqrt(-4*-11*3)
+
+
+cf<-c("Liz","Carmen")
+sample(cf,1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
